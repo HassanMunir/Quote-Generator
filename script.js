7df9f4d24bd111c370e4038e7ff7c7cdc2f57a60
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2022-02-26 21:37:27 +0500

    Commit

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
-function newQuote (){
+function newQuote() {
   // Pick a New Quote from apiQuotes Array
-  const quote = apiQuotes [Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check if the author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote Length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -15,11 +34,20 @@ async function getQuotes() {
     apiQuotes = await response.json();
     // console.log(apiQuotes);   - Replace with newQuote()
     newQuote();
-
   } catch (error) {
     // Catch Error Here
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
